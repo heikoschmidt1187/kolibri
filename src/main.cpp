68aa0a3aaeb9985dca5c14f019e8de6cc43ec702
Heiko Schmidt <dev@heikoschmidt.info>
2025-04-11 18:56:10 +0200

    Fix typo

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 
 #define INTERNAL_LED_GPIO 13U
 #define RED_LED_GPIO 5U
-#define GREE_LED_GPIO 6U
+#define GREEN_LED_GPIO 6U
 
 #define BATTERY_VOLTAGE_INPUT_PIN 15U
 #define BATTERY_VOLTAGE_INPUT_RESOLUTION_BITS 10U
@@ -39,12 +39,12 @@ void setup()
     // configure LED pins
     pinMode(INTERNAL_LED_GPIO, OUTPUT);
     pinMode(RED_LED_GPIO, OUTPUT);
-    pinMode(GREE_LED_GPIO, OUTPUT);
+    pinMode(GREEN_LED_GPIO, OUTPUT);
 
     // switch on activity LED and signal start of setup
     digitalWrite(INTERNAL_LED_GPIO, HIGH);
     digitalWrite(RED_LED_GPIO, HIGH);
-    digitalWrite(GREE_LED_GPIO, LOW);
+    digitalWrite(GREEN_LED_GPIO, LOW);
 
     // configure I2C and start bus
     Wire.setClock(I2C_FREQ_IN_HZ);
@@ -59,7 +59,7 @@ void setup()
 
     // signal end of setup
     digitalWrite(RED_LED_GPIO, LOW);
-    digitalWrite(GREE_LED_GPIO, HIGH);
+    digitalWrite(GREEN_LED_GPIO, HIGH);
 }
 
 void loop()
@@ -74,9 +74,9 @@ void loop()
                   gyroscope.GetRollRate(), gyroscope.GetPitchRate(), gyroscope.GetYawRate());
 
     // check channel values
-    receiver.Process();
-    for (auto i = 0; i < receiver.GetActualChannelCount(); ++i)
-        Serial.printf("Channel %d: %f\n", i, receiver.GetChannelValue(i));
+    // receiver.Process();
+    // for (auto i = 0; i < receiver.GetActualChannelCount(); ++i)
+    // Serial.printf("Channel %d: %f\n", i, receiver.GetChannelValue(i));
 
     delay(50);
 }
