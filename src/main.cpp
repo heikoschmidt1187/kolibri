f5d8e2f1685534a7f5c455cf365caa8c433f97b5
Heiko Schmidt <dev@heikoschmidt.info>
2025-04-12 18:57:26 +0200

    Clean code format

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@
 #include <config.h>
 
 BatteryMonitor batteryMonitor(BATTERY_VOLTAGE_INPUT_PIN,
-                              BATTERY_CURRENT_INPUT_PIN,
-                              ADC_INPUT_RESOLUTION_BITS,
-                              BATTERY_VOLTAGE_DIVIDER_RATIO,
-                              BATTERY_CURRENT_DIVIDER_RATIO,
-                              (float)CYCLE_TIME_MS / 1000.F);
+							  BATTERY_CURRENT_INPUT_PIN,
+							  ADC_INPUT_RESOLUTION_BITS,
+							  BATTERY_VOLTAGE_DIVIDER_RATIO,
+							  BATTERY_CURRENT_DIVIDER_RATIO,
+							  (float)CYCLE_TIME_MS / 1000.F);
 Gyroscope gyroscope(MCP6050_BUS_ID);
 Receiver receiver(RECEIVER_PIN);
 
@@ -22,72 +22,71 @@ static auto batteryLowCurOutput = LOW;
 
 void setup()
 {
-    // configure serial monitor
-    Serial.begin(SERIAL_BAUDRATE);
-    Serial.println("Welcome to Kolibri v" VERSION);
-    Serial.println("=======================");
+		// configure serial monitor
+		Serial.begin(SERIAL_BAUDRATE);
+		Serial.println("Welcome to Kolibri v" VERSION);
+		Serial.println("=======================");
 
-    // configure LED pins
-    pinMode(INTERNAL_LED_GPIO, OUTPUT);
-    pinMode(RED_LED_GPIO, OUTPUT);
-    pinMode(GREEN_LED_GPIO, OUTPUT);
+		// configure LED pins
+		pinMode(INTERNAL_LED_GPIO, OUTPUT);
+		pinMode(RED_LED_GPIO, OUTPUT);
+		pinMode(GREEN_LED_GPIO, OUTPUT);
 
-    // switch on activity LED and signal start of setup
-    digitalWrite(INTERNAL_LED_GPIO, HIGH);
-    digitalWrite(RED_LED_GPIO, HIGH);
-    digitalWrite(GREEN_LED_GPIO, LOW);
+		// switch on activity LED and signal start of setup
+		digitalWrite(INTERNAL_LED_GPIO, HIGH);
+		digitalWrite(RED_LED_GPIO, HIGH);
+		digitalWrite(GREEN_LED_GPIO, LOW);
 
-    // configure I2C and start bus
-    Wire.setClock(I2C_FREQ_IN_HZ);
-    Wire.begin();
-    delay(250U);
+		// configure I2C and start bus
+		Wire.setClock(I2C_FREQ_IN_HZ);
+		Wire.begin();
+		delay(250U);
 
-    // init Gyro
-    gyroscope.Init();
+		// init Gyro
+		gyroscope.Init();
 
-    // init receiver
-    receiver.Init();
+		// init receiver
+		receiver.Init();
 
-    // init battery monitor
-    if (batteryMonitor.Init() == BatteryMonitor::BatteryLevel::BAT_LOW)
-        batteryLow = true;
+		// init battery monitor
+		if (batteryMonitor.Init() == BatteryMonitor::BatteryLevel::BAT_LOW)
+				batteryLow = true;
 
-    // signal end of setup
-    if (!batteryLow)
-        digitalWrite(RED_LED_GPIO, LOW);
+		// signal end of setup
+		if (!batteryLow)
+				digitalWrite(RED_LED_GPIO, LOW);
 
-    digitalWrite(GREEN_LED_GPIO, HIGH);
+		digitalWrite(GREEN_LED_GPIO, HIGH);
 }
 
 void loop()
 {
-    // check battery and visualize
-    auto level = batteryMonitor.Process();
-    if (batteryLow)
-    {
-        if (batteryLowBlinkCtr == 0U)
-        {
-            batteryLowBlinkCtr = 500U / CYCLE_TIME_MS;
-            batteryLowCurOutput = batteryLowCurOutput == HIGH ? LOW : HIGH;
-        }
-        batteryLowBlinkCtr--;
-        digitalWrite(RED_LED_GPIO, batteryLowCurOutput);
-    }
-    else
-    {
-        batteryLow = (level == BatteryMonitor::BAT_LOW);
-    }
+		// check battery and visualize
+		auto level = batteryMonitor.Process();
+		if (batteryLow) {
+				if (batteryLowBlinkCtr == 0U) {
+						batteryLowBlinkCtr = 500U / CYCLE_TIME_MS;
+						batteryLowCurOutput =
+							batteryLowCurOutput == HIGH ? LOW : HIGH;
+				}
+				batteryLowBlinkCtr--;
+				digitalWrite(RED_LED_GPIO, batteryLowCurOutput);
+		} else {
+				batteryLow = (level == BatteryMonitor::BAT_LOW);
+		}
 
-    // check rates
-    gyroscope.Process();
-    // Serial.printf("Roll-Rate: %f °/s | Pitch-Rate: %f °/s | Yaw-Rate: %f °/s\n",
-    // gyroscope.GetRollRate(), gyroscope.GetPitchRate(), gyroscope.GetYawRate());
+		// check rates
+		gyroscope.Process();
+		Serial.printf(
+			"Roll-Rate: %f °/s | Pitch-Rate: %f °/s | Yaw-Rate: %f °/s\n",
+			gyroscope.GetRollRate(), gyroscope.GetPitchRate(),
+			gyroscope.GetYawRate());
 
-    // check channel values
-    receiver.Process();
-    // for (auto i = 0; i < receiver.GetActualChannelCount(); ++i)
-    // Serial.printf("Channel %d: %f\n", i, receiver.GetChannelValue(i));
+		// check channel values
+		receiver.Process();
+		// for (auto i = 0; i < receiver.GetActualChannelCount(); ++i)
+		// Serial.printf("Channel %d: %f\n", i, receiver.GetChannelValue(i));
 
-    // TODO: use cycle taking calculation into consideration
-    delay(4);
+		// TODO: use cycle taking calculation into consideration
+		delay(4);
 }
